420cde0ff48d9daf96cd9fab40d8e5eb89ba243a
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-10-20 15:01:20 -0400

    finished setup/ class file creation, cehck again later

---
+++ MAD9137_Midterm/NewEventViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewEventViewController.swift
+//  MAD9137_Midterm
+//
+//  Created by Ravi Rachamalla on 2020-10-20.
+//  Copyright © 2020 rav. All rights reserved.
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
